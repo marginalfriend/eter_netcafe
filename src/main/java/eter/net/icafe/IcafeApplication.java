414c2b26c468de7bfcc061267f8d907b8e9d8375
Abdullah <abdullahpsg@gmail.com>
2024-07-23 08:25:25 +0700

    [INITIAL] initial commit

---
+++ src/main/java/eter/net/icafe/IcafeApplication.java
@@ -0,0 +1,13 @@
+package eter.net.icafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eter/net/icafe/IcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package eter.net.icafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
